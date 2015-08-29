243e17b59a8bce3dc8c71e21ec0d2aed715c91d7
Alberto Iglesias Gallego <alberto.uchiha@gmail.com>
2015-08-29 22:07:33 +0200

    First refactor of the app

--- server.js
+++ server.js
@@ -1,30 +1,68 @@
+//Modules
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+//Own files
 var config = require('./config.json');
 var utils = require('./utils.js');
-
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var cors = require('cors');
-var authCtrl = require('./auth');
+var authentication = require('./modules/authentication');
 var middleware = require('./middleware');
 
+//Models
+var usersModel = require('./models/user');
+
+//Variables
 var app = express();
+var router = express.Router();
+var exports = module.exports = {};
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-app.set('port', config.port);
 
-require('./models/user');
-
-var router = express.Router();
-router.post('/auth/signup', authCtrl.emailSignup);
-router.post('/auth/login', authCtrl.emailLogin);
-//router.get('/private',middleware.ensureAuthenticated, function(req, res) {...} );
-
-mongoose.connect(config.mongodb.host, function(err) {
-    app.listen(app.get('port'), function(){
-        console.log(
-            utils.getInitServerMessage(config)
-        );
-    });
+//Routes
+//router.post('/auth/signup', authentication.signUp);
+//router.post('/auth/signin', authentication.signIn);
+router.get('/',function(req, res){
+    res.status(200);
 });
+
+function start(){
+    var server =  app.listen(app.get('port'), function(){
+        console.log(utils.getInitServerMessage(config));
+        startMongoose(function(err,mongoServer){
+            if(err!==undefined) {
+                console.error("Shutting down watchdog server - Reason: \n\n\t" + err.toString());
+                stop(server);
+            }
+        });
+    });
+
+    return server;
+}
+
+function startMongoose(callback){
+    var mongoServer = mongoose.connect(config.mongodb.host, function(err) {
+        if(err===undefined) {
+            console.info("MongoDB is running.");
+        }
+        callback(err,mongoServer);
+    });
+}
+
+function stopMongoose(instance){
+    isntance.connection.close();
+}
+
+function stop(instance){
+    instance.close();
+}
+
+module.exports = {
+    start : start,
+    stop : stop,
+    startMongoose: startMongoose,
+    stopMongoose: stopMongoose
+};
