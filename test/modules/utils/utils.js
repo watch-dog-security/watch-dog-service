53ce5925067470f4d394aa20aceffc60e93f979f
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-03-21 12:48:45 +0100

    Add test utils from modules
    Add test from modules, this module just have two functions like instance manager and
    get console.log with green tick. It's a small module but is necessary to test everything
    to improve the code. In fact, i add to the project istanbul library to pass the coverage and
    sinon.

---
+++ test/modules/utils/utils.js
@@ -0,0 +1,43 @@
+'use strict';
+
+let assert = require('assert');
+let utils = require('./../../../modules/utils/utils.js');
+let sinon = require('sinon');
+let chai = require('chai');
+let expect = chai.expect;
+
+describe('Utils module', () => {
+	describe('Check Function getArrayResponseForInstances', () => {
+		it('should return the correct instance and msg',(done) => {
+			var instance = {
+				name: 'service test',
+				instance: 'instancia',
+				msg: 'mensaje de la instancia'
+			};
+			var returnedInstance = utils.getArrayResponseForInstances(instance.name, instance.instance, instance.msg);
+
+			assert.equal(instance.name, returnedInstance.name);
+			assert.equal(instance.instance, returnedInstance.instance);
+			assert.equal(instance.msg, returnedInstance.msg);
+			done();
+		})
+	});
+
+	describe('Check Function consoleLogWithTick', () => {
+		beforeEach(function() {
+			sinon.spy(console, 'log');
+		});
+
+		afterEach(function() {
+			console.log.restore();
+		});
+
+		it('should log to console with the tick',(done) => {
+			var msg = 'hola mundo';
+
+			utils.consoleLogWithTick(msg);
+			expect(console.log).to.be.called;
+			done();
+		})
+	});
+});
