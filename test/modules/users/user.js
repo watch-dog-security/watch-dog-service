7daa143e4cebf07a8c0d3fb87a778f127f423fee
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-03-28 10:16:36 +0200

    Resolve Date problems on user.js test module
    Resolve Date problems on user.js test module when user is created, is imposible to have the exactly Date on two different objects that are created in different times.

--- test/modules/users/user.js
+++ test/modules/users/user.js
@@ -218,7 +218,13 @@ describe('User module', () => {
 			var mockedCorrectUser = new User(mock.correctUser);
 			mockedCorrectUser._doc._id = userFromJson._doc._id;
 
-			assert.deepEqual(JSON.stringify(userFromJson), JSON.stringify(mockedCorrectUser));
+			assert(mockedCorrectUser.created_at);
+			assert(userFromJson.created_at);
+
+			mockedCorrectUser.created_at = '';
+			userFromJson.created_at = '';
+
+			assert.equal(JSON.stringify(userFromJson), JSON.stringify(mockedCorrectUser));
 
 			done();
 		});
