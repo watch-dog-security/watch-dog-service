683b4b29448e1a75aad0ec9827a41f2c329bb5ed
Alberto  Iglesias Gallego <alberto.uchiha@gmail.com>
2016-04-26 15:50:18 +0200

    Add error handler test file

---
+++ test/middlewares/error/handler.js
@@ -0,0 +1,37 @@
+'use strict';
+
+let express = require('express');
+let assert = require('assert');
+let expect = require('chai').expect;
+let sinon  = require('sinon');
+let httpMocks  = require('node-mocks-http');
+let handlerError = require('./../../../middlewares/error/handler');
+let AppError = require('./../../../modules/error/manager');
+
+describe('Middleware handler error: ', () => {
+
+
+	it('Should thrown an Error ""', (done) => {
+		let app = express();
+		let res = httpMocks.createResponse(app);
+		let req = httpMocks.createRequest();
+
+		handlerError(AppError('EMAIL_NOT_CORRECT'),req, res, () => {});
+
+		assert.equal(res.statusCode, AppError('EMAIL_NOT_CORRECT').code);
+		done()
+	});
+
+	it('Should call next when error is null or entry',(done) => {
+		let app = express();
+		let res = httpMocks.createResponse(app);
+		let req = httpMocks.createRequest();
+
+		let spy = sinon.spy(() => {
+			expect(spy.called).to.be.true;
+			done();
+		});
+
+		handlerError(null, req, res, spy);
+	})
+});
